225767782f4694725c8a6a4137b78a32f773ae88
ramanboff <bayramofframan@gmail.com>
2023-09-17 11:00:01 +0400

    about Page section-2

--- src/pages/Site/About/About.jsx
+++ src/pages/Site/About/About.jsx
@@ -3,12 +3,12 @@ import "./About.scss";
 // import { BiChevronRight } from "react-icons/bi";
 
 import { Link } from "react-router-dom";
-import { AiFillCheckSquare, AiFillCheckCircle } from "react-icons/Ai";
+import { AiFillCheckCircle, AiFillCheckSquare } from "react-icons/Ai";
 import Loading from "../../../Components/Loading/Loading";
 import { useEffect, useState } from "react";
 import { FaAngleRight } from "react-icons/fa";
 import Carusel1 from "../../../Components/Carusel1/Carusel1";
-import { BsFillCheckSquareFill } from "react-icons/Bs";
+import { BsFillCheckSquareFill, BsFillPatchCheckFill } from "react-icons/Bs";
 export default function About() {
   const [loading, setLoading] = useState(false);
   useEffect(() => {
@@ -43,56 +43,55 @@ export default function About() {
 
           {/* https://themes.hibootstrap.com/varn/wp-content/uploads/2020/01/about-img1-1-1.png */}
 
-         <div className="container">
-         <div className="section-1">
-            <div className="left">
-              <img
-                src=" https://themes.hibootstrap.com/varn/wp-content/uploads/2020/01/about-img1-1-1.png"
-                alt=""
-              />
-            </div>
-            <div className="right">
-              <p className="title">About us</p>
-              <h2 className="header">We are digital explorers </h2>
-              <p className="description">
-                We believe brand interaction is key in communication. Real
-                innovations and a positive customer experience are the heart of
-                successful communication.
-              </p>
-              <div className="cards-container">
-              <div className="left-cards-container">
-                <div className="card">
-                  <BsFillCheckSquareFill  className="icon-about" />
-                  <p>SEO Marketing</p>
-                </div>
-                <div className="card">
-                  <BsFillCheckSquareFill className="icon-about" />
-                  <p>SEO Marketing</p>
-                </div>
-                <div className="card">
-                  <BsFillCheckSquareFill className="icon-about" />
-                  <p>SEO Marketing</p>
+          <div className="container">
+            <div className="section-1">
+              <div className="left">
+                <img
+                  src=" https://themes.hibootstrap.com/varn/wp-content/uploads/2020/01/about-img1-1-1.png"
+                  alt=""
+                />
+              </div>
+              <div className="right">
+                <p className="title">About us</p>
+                <h2 className="header">We are digital explorers </h2>
+                <p className="description">
+                  We believe brand interaction is key in communication. Real
+                  innovations and a positive customer experience are the heart
+                  of successful communication.
+                </p>
+                <div className="cards-container">
+                  <div className="left-cards-container">
+                    <div className="card">
+                      <BsFillCheckSquareFill className="icon-about" />
+                      <p>SEO Marketing</p>
+                    </div>
+                    <div className="card">
+                      <BsFillCheckSquareFill className="icon-about" />
+                      <p>SEO Marketing</p>
+                    </div>
+                    <div className="card">
+                      <BsFillCheckSquareFill className="icon-about" />
+                      <p>SEO Marketing</p>
+                    </div>
+                  </div>
+                  <div className="right-cards-container">
+                    <div className="card">
+                      <BsFillCheckSquareFill className="icon-about" />
+                      <p>SEO Marketing</p>
+                    </div>
+                    <div className="card">
+                      <BsFillCheckSquareFill className="icon-about" />
+                      <p>SEO Marketing</p>
+                    </div>
+                    <div className="card">
+                      <BsFillCheckSquareFill className="icon-about" />
+                      <p>SEO Marketing</p>
+                    </div>
+                  </div>
                 </div>
               </div>
-              <div className="right-cards-container">
-                <div className="card">
-                  <BsFillCheckSquareFill className="icon-about" />
-                  <p>SEO Marketing</p>
-                </div>
-                <div className="card">
-                  <BsFillCheckSquareFill className="icon-about" />
-                  <p>SEO Marketing</p>
-                </div>
-                <div className="card">
-                  <BsFillCheckSquareFill className="icon-about" />
-                  <p>SEO Marketing</p>
-                </div>
-              </div>
             </div>
-            </div>
-          
           </div>
-         </div>
 
           {/* <div className="aboutpage__bottom">
               <div className="aboutpage__bottom__left">
@@ -179,9 +178,86 @@ export default function About() {
 
           <div className="container">
             <section className="section-2">
-             <div className="card">
-              
-             </div>
+              <div className="cardsContainer">
+                <div className="card">
+                  <h2>Our History</h2>
+                  <div className="devider"></div>
+                  <p className="description">
+                    Real innovations and positive customer experience are the
+                    heart of successful communication.
+                  </p>
+                  <div className="cardListsContainer">
+                    <div className="cardList">
+                     <div className="icon-container"> <AiFillCheckCircle className="list-icon" /></div>
+                      <p>Activate listening</p>
+                    </div>
+                    <div className="cardList">
+                      <AiFillCheckCircle className="list-icon" />
+                      <p>Brilliant minds</p>
+                    </div>
+                    <div className="cardList">
+                      <AiFillCheckCircle className="list-icon" />
+                      <p>Better, best, wow!</p>
+                    </div>
+                    <div className="cardList">
+                      <AiFillCheckCircle className="list-icon" />
+                      <p>Branding it better!</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="card">
+                  <h2>Our History</h2>
+                  <div className="devider"></div>
+                  <p className="description">
+                    Real innovations and positive customer experience are the
+                    heart of successful communication.
+                  </p>
+                  <div className="cardListsContainer">
+                    <div className="cardList">
+                      <AiFillCheckCircle className="list-icon" />
+                      <p>Activate listening</p>
+                    </div>
+                    <div className="cardList">
+                      <AiFillCheckCircle className="list-icon" />
+                      <p>Brilliant minds</p>
+                    </div>
+                    <div className="cardList">
+                      <AiFillCheckCircle className="list-icon" />
+                      <p>Better, best, wow!</p>
+                    </div>
+                    <div className="cardList">
+                      <AiFillCheckCircle className="list-icon" />
+                      <p>Branding it better!</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="card">
+                  <h2>Our History</h2>
+                  <div className="devider"></div>
+                  <p className="description">
+                    Real innovations and positive customer experience are the
+                    heart of successful communication.
+                  </p>
+                  <div className="cardListsContainer">
+                    <div className="cardList">
+                      <AiFillCheckCircle className="list-icon" />
+                      <p>Activate listening</p>
+                    </div>
+                    <div className="cardList">
+                      <AiFillCheckCircle className="list-icon" />
+                      <p>Brilliant minds</p>
+                    </div>
+                    <div className="cardList">
+                      <AiFillCheckCircle className="list-icon" />
+                      <p>Better, best, wow!</p>
+                    </div>
+                    <div className="cardList">
+                      <AiFillCheckCircle className="list-icon" />
+                      <p>Branding it better!</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </section>
           </div>
 
