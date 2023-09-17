a91d9675df1d65167e3ea51be95e02dbb6da91bc
ramanboff <bayramofframan@gmail.com>
2023-09-17 17:47:16 +0400

    blogs page  heading created

--- src/pages/Site/Blogs/Blogs.jsx
+++ src/pages/Site/Blogs/Blogs.jsx
@@ -1,10 +1,11 @@
 
 import React, { useEffect, useState } from "react";
 import "./Blogs.scss";
-import { BiChevronRight } from 'react-icons/Bi'
+
 import Loading from "../../../Components/Loading/Loading";
 // import { AiOutlineSearch } from "react-icons/ai";
 import { Link } from 'react-router-dom';
+import { FaAngleRight } from "react-icons/fa";
 export default function Blogs() {
   const [loading, setLoading] = useState(false);
   useEffect(() => {
@@ -20,90 +21,23 @@ export default function Blogs() {
         <Loading />
       ) : (
         <div>
-          <div className='blog_heading'>
-            <h2>Blogs</h2>
-            <div className='lines'></div>
-            <ul><li><Link className='home' to='/'>Home</Link></li><BiChevronRight className='iconss' /><li><Link className='contact' to='/blogs'>Blogs</Link></li></ul>
-          </div>
-          <div className="blogs">
-
-            <div className="blogs__cards__column">
-              <div className="blogs__card__width">
-                <div className="blogs__card">
-                  <img src="https://themes.hibootstrap.com/varn/wp-content/uploads/2020/01/blog-image-2-1-1.jpg" />
-                  <div className="hidden-overflow">
-                    <h3>How to use music to boost your business</h3>
-                    <p>
-                      Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div className="blogs__card__width">
-                <div className="blogs__card">
-                  <img src="https://themes.hibootstrap.com/varn/wp-content/uploads/2020/01/blog-image-2-1-1.jpg" />
-                  <div className="hidden-overflow">
-                    <h3>How to use music to boost your business</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-                  </div>
-                </div>
-              </div>
-              <div className="blogs__card__width">
-                <div className="blogs__card">
-                  <img src="https://themes.hibootstrap.com/varn/wp-content/uploads/2020/01/blog-image-2-1-1.jpg" />
-                  <div className="hidden-overflow">
-                    <h3>How to use music to boost your business</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-                  </div>
-                </div>
-              </div>
-              <div className="blogs__card__width">
-                <div className="blogs__card">
-                  <img src="https://themes.hibootstrap.com/varn/wp-content/uploads/2020/01/blog-image-2-1-1.jpg" />
-                  <div className="hidden-overflow">
-                    <h3>How to use music to boost your business</h3>
-                    <p>
-                      Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                    </p>
-                  </div>
-                </div>
+          <div className="blogHeader">
+            <div className="container">
+              <div className="blogTitle">
+                <h2>
+                  B<span>lo</span>g
+                </h2>
               </div>
             </div>
-            <div className="blogs__mini__cards__column">
-              <div className="blogs__search">
-                <input placeholder="Search" />
-                {/* <AiOutlineSearch className="blogs__search__icon" /> */}
-              </div>
-              <div className="blogs__mini__cards">
-                <h4>Recent posts</h4>
-                <hr />
-                <div className="blogs__mini__card">
-                  <img src="https://themes.hibootstrap.com/varn/wp-content/uploads/2020/01/blog-image-2-1-1.jpg" />
-                  <div className="blogs__mini__card__content">
-                    <p>April 25, 2020</p>
-                    <h4>Making Peace With The Feast Or Famine</h4>
-                  </div>
-                </div>
-                <hr />
-                <div className="blogs__mini__card">
-                  <img src="https://themes.hibootstrap.com/varn/wp-content/uploads/2020/01/blog-image-2-1-1.jpg" />
-                  <div className="blogs__mini__card__content">
-                    <p>April 25, 2020</p>
-                    <h4>Making Peace With The Feast Or </h4>
-                  </div>
-                </div>
-                <hr />
-                <div className="blogs__mini__card">
-                  <img src="https://themes.hibootstrap.com/varn/wp-content/uploads/2020/01/blog-image-2-1-1.jpg" />
-                  <div className="blogs__mini__card__content">
-                    <p>April 25, 2020</p>
-                    <h4>Making Peace With The Feast Or </h4>
-                  </div>
-                </div>
-                <hr />
-              </div>
+            <div className="blogLinks">
+              <Link to="/home" className="blogLink">
+                Home
+              </Link>
+              <FaAngleRight className="angleIcon" />
+              <Link to="blog">Blog</Link>
             </div>
           </div>
+        
         </div>
 
       )}
