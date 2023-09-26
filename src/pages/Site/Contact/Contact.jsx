ceeeab858210ea95aacfff84b02939b31de17b79
ramanboff <bayramofframan@gmail.com>
2023-09-26 14:23:06 +0400

    Contact Page

--- src/pages/Site/Contact/Contact.jsx
+++ src/pages/Site/Contact/Contact.jsx
@@ -43,66 +43,11 @@ export default function Contact() {
             </div>
           </div>
           {/*  HEADER LINK END  */}
-         <div className='home__contact__cards'>
-          <div className='home__contact__cards__card'>
-             <div className='home__contact__cards__card__icon'>
-              <MdOutlineMailOutline className='icon1'/>
-             </div>
-             <h3>Email Here</h3>
-             <p>loremipsum@gmail.com</p>
-             <p>hesen@gmail.com</p>
-          </div>
-          <div className='home__contact__cards__card'>
-          <div className='home__contact__cards__card__icon'>
-           <SlLocationPin className='icon1'/>
-          </div>
-          <h3>Location Here</h3>
-          <p>&nbsp;2750 Quadra Street Victoria Road, New York, Canada  &nbsp;  </p>
-     
-          </div>
-       <div className='home__contact__cards__card'>
-       <div className='home__contact__cards__card__icon'>
-        <BsPhone className='icon1'/>
-       </div>
-       <h3>Phone Here</h3>
-       <p>+123 67 5634 8989</p>
-       <p>+2345 67 8912</p>
-       </div>
-       </div>
+       
       </div>
           
         <div className='container'>
-          <div className='contact'>
-              <div className='contact__content'>
-                <span>Contact us</span>
-                <h2>Do you have any questions?</h2>
-                <p>Real innovations and a positive customer experience are the heart of successful communication. No fake products and services.</p>
-              </div>
-              <div className='contact__bottom'>
-               <div className='contact__bottom__left'>
-                 <img src='https://themes.hibootstrap.com/varn/wp-content/uploads/2020/01/faq-contact-1-1.png'></img>
-               </div>
-               <div class='contact__bottom__right'>
-               <div class='content__bottom__right__top'>
-                 <div class='column'>
-                   <p><input type='text' placeholder='Name'/></p>
-                   <input type='email' placeholder='Email'/>
-                 </div>
-                 <div class='column'>
-                   <input type='tel' placeholder='Phone'/>
-                   <input type='text' placeholder='Subject'/>
-                 </div>
-               </div>
-               <div>
-                 <textarea className='area' placeholder='Your Message'></textarea>
-               </div>
-               <div>
-                 <button type='submit'>Send Message</button>
-               </div>
-             </div>
-             
-            </div>             
-          </div>
+         
         
         </div>
          
