22eacf1f513e233a7e1d264194288e43e8cea7ba
ramanboff <bayramofframan@gmail.com>
2023-10-03 23:04:14 +0400

    Pagination of Blog Page

--- src/Components/BlogList/BlogList.jsx
+++ src/Components/BlogList/BlogList.jsx
@@ -1,28 +1,42 @@
-
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-
-
 const BlogList = ({ blogs }) => {
+  const apiUrl = import.meta.env.VITE_API_URL;
+  // const { data: blogs, error } = UseFetch(`${apiUrl}blogs`);
+  const [posts, setPosts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPage] = useState(9);
 
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await axios.get(`${apiUrl}blogs`);
+      setPosts(res.data);
+    };
+    fetchPosts()
+  },[]);
+  console.log(posts)
 
-// const [searchCard, setSearchCard] = useState("")
-// console.log(searchCard);
- 
-return (
+/*   Get Current posts */
+const indexOfLastPost = currentPage * postsPerPage
+const indexOfFirstPost = indexOfLastPost - postsPerPage
+const currentPosts =  posts.slice(indexOfFirstPost, indexOfLastPost)
+
+  return (
     <div className="cards-container">
-      {blogs.map((blog) => (
+      {currentPosts.map((blog) => (
         <Link to={`/blogs/${blog.id}`} key={blog.id}>
-        <div className="card" key={blog.id}>
-          <div className="cardImgContainer">
-          {/* r */}
-            <img src={blog.photo} />
+          <div className="card" key={blog.id}>
+            <div className="cardImgContainer">
+              {/* r */}
+              <img src={blog.photo} />
+            </div>
+            <div className="bottom-content">
+              <h2>{blog.title}</h2>
+              {/* <p>{blog.short_descriptions}</p> */}
+            </div>
           </div>
-          <div className="bottom-content">
-            <h2>{blog.title}</h2>
-            {/* <p>{blog.short_descriptions}</p> */}
-          </div>
-        </div>
         </Link>
       ))}
     </div>
