6a0df7f8a2cc77fb99bfa59e352d3937a2168e88
ramanboff <bayramofframan@gmail.com>
2023-10-17 00:37:47 +0400

    Home Slider Added url and  made slider dynamic

--- src/Components/MySlider/MySlider.jsx
+++ src/Components/MySlider/MySlider.jsx
@@ -5,7 +5,11 @@ import "swiper/css/pagination";
 import "./MySlider.scss";
 import { Link } from "react-router-dom";
 import { Pagination } from "swiper/modules";
+import UseFetch from "../../UseFetch";
 function MySlider() {
+  const apiUrl = import.meta.env.VITE_API_URL;
+  const { data: slider } = UseFetch(`${apiUrl}core_api/slider`);
+
   const sliderRef = useRef(null);
   return (
     <div className="slider-container">
@@ -18,93 +22,31 @@ function MySlider() {
         autoplay={{ delay: 3000 }}
         onSwiper={(swiper) => (sliderRef.current = swiper)}
       >
-        <SwiperSlide>
-          <div className="slider1">
-            <img
-              className="slider1__img"
-              src="https://img.freepik.com/premium-vector/white-simple-abstract-wallpaper_393744-50.jpg?w=2000"
-              alt="Slide 1"
-            />
+        {slider &&
+          slider.map(({slider_image,slider_header,content}) => (
+            <SwiperSlide>
+              <div className="slider1">
+                <img
+                  className="slider1__img"
+                  src={slider_image}
+                  alt="Slide 1"
+                />
 
-           <div className="container">
-           <div className="slider1__content">
-              <h1>
-                Secure IT solutions<br></br> for a more secure environment!{" "}
-              </h1>
-              <p className="">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                <br />
-                do site Phasellus nec feugiat nunc, non commodo diam. non
-                commodo diam elit sit. We work hand-in-hand with
-                industry-leading brands to help redefine the possibilities and
-                potential of digital engagements.
-              </p>
-              <Link to="/contact">
-                {" "}
-                <button>Get Started</button>
-              </Link>
-            </div>
-           </div>
-          </div>
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <div className="slider1">
-            <img
-              className="slider1__img"
-              src="https://wallpaperset.com/w/full/8/8/0/462749.jpg"
-              alt="Slide 1"
-            />
-            <div className="container">
-              <div className="slider1__content">
-                <h1>
-                  Secure IT solutions<br></br> for a more secure environment!{" "}
-                </h1>
-                <p className="">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  <br />
-                  do site Phasellus nec feugiat nunc, non commodo diam. non
-                  commodo diam elit sit. We work hand-in-hand with
-                  industry-leading brands to help redefine the possibilities and
-                  potential of digital engagements.
-                </p>
-                <Link to="/contact">
-                  {" "}
-                  <button>Get Started</button>
-                </Link>
+                <div className="container">
+                  <div className="slider1__content">
+                    <h1>{slider_header}</h1>
+                    <p className="">
+                      {content}
+                    </p>
+                    <Link to="/contact">
+                      {" "}
+                      {/* <button>Get Started</button> */}
+                    </Link>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <div className="slider1">
-            <img
-              className="slider1__img"
-              src="https://cdn.wallpapersafari.com/55/13/4KtLmN.jpg"
-              alt="Slide 1"
-            />
-            <div className="container">
-              <div className="slider1__content">
-                <h1>
-                  Secure IT solutions<br></br> for a more secure environment!{" "}
-                </h1>
-                <p className="">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  <br />
-                  do site Phasellus nec feugiat nunc, non commodo diam. non
-                  commodo diam elit sit. We work hand-in-hand with
-                  industry-leading brands to help redefine the possibilities and
-                  potential of digital engagements.
-                </p>
-                <Link to="/contact">
-                  {" "}
-                  <button>Get Started</button>
-                </Link>
-              </div>
-            </div>
-          </div>
-        </SwiperSlide>
+            </SwiperSlide>
+          ))}
       </Swiper>
     </div>
   );
