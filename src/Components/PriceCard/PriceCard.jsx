b9784f37e241e289d22e3bc3f185f0f2b2716986
ramanboff <bayramofframan@gmail.com>
2023-10-16 18:59:31 +0400

    done as dinamic with backend

--- src/Components/PriceCard/PriceCard.jsx
+++ src/Components/PriceCard/PriceCard.jsx
@@ -1,21 +1,36 @@
 import { TiTick } from "react-icons/ti";
+import UseFetch from "../../UseFetch";
 
-
-const PriceCard = ({ title, price, serviceItems, color }) => {
-
+const PriceCard = ({ title, price, serviceItems, color, period }) => {
+  const apiUrl = import.meta.env.VITE_API_URL;
+  const { data: priceProperty } = UseFetch(
+    `${apiUrl}service_api/package_property/`
+  );
 
   return (
     <div className="cardsContainer__card">
-      <h2 style={{backgroundColor: color}} className="cardsContainer__cardHeader">{title}</h2>
-      <p className="cardsContainer__price">{price}</p>
+      <h2
+        style={{ backgroundColor: color }}
+        className="cardsContainer__cardHeader"
+      >
+        {title}
+      </h2>
+
+      <div className="cardsContainer__pricePeriodContainer">
+        <p className="cardsContainer__exchangeRate">$</p>
+        <span className="cardsContainer__price">{price}</span>
+        <p className="cardsContainer__period">/ {period}</p>
+      </div>
+
       <div className="cardsContainer__devider"></div>
       <div className="cardsContainer__cardContentContainer">
-        {serviceItems.map((item, index) => (
-          <div className="cardsContainer__itemsContainer" key={index}>
-            <p className="cardsContainer__items">{item}</p>
-            <TiTick className="cardsContainer__tick-icon" />
-          </div>
-        ))}
+        {priceProperty &&
+          priceProperty.map((item, index) => (
+            <div className="cardsContainer__itemsContainer" key={index}>
+              <p className="cardsContainer__items">{item.property_name}</p>
+              <TiTick className="cardsContainer__tick-icon" />
+            </div>
+          ))}
       </div>
       <div className="cardsContainer__devider"></div>
       <a className="cardsContainer__link">Get Started</a>
