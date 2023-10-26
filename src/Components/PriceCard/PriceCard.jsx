13b922b57471b16920e5717508d07b430c42d304
ramanboff <bayramofframan@gmail.com>
2023-10-27 00:17:26 +0400

    PriceCards component made dinamic

--- src/Components/PriceCard/PriceCard.jsx
+++ src/Components/PriceCard/PriceCard.jsx
@@ -1,53 +1,47 @@
 import { TiTick } from "react-icons/ti";
 import UseFetch from "../../UseFetch";
-import { AiOutlineClose } from "react-icons/ai";
+import { AiOutlineClose } from "react-icons/Ai";
 
-const PriceCard = ({
-  title,
-  price,
-  property_name,
-  color,
-  period,
-  package_properties,
-}) => {
+const PriceCard = ({ title, price, color, period, property ,symbol}) => {
   const apiUrl = import.meta.env.VITE_API_URL;
-  const { data: priceProperty } = UseFetch(
-    `${apiUrl}service_api/package_property/`
-  );
+
 
   return (
-    <div className="cardsContainer__card">
-      <h2
-        style={{ backgroundColor: color }}
-        className="cardsContainer__cardHeader"
-      >
-        {title}
-      </h2>
 
-      <div className="cardsContainer__pricePeriodContainer">
-        <p className="cardsContainer__exchangeRate">$</p>
-        <span className="cardsContainer__price">{price}</span>
-        <p className="cardsContainer__period">/ {period}</p>
-      </div>
+     
+        <div className="cardsContainer__card">
+          <h2
+            style={{ backgroundColor: color }}
+            className="cardsContainer__cardHeader"
+          >
+            {title}
+          </h2>
 
-      <div className="cardsContainer__devider"></div>
-      <div className="cardsContainer__cardContentContainer">
-        {package_properties &&
-          package_properties.map((item, index) => (
-            <div className="cardsContainer__itemsContainer" key={index}>
-              <p className="cardsContainer__items">{item.property_name}</p>
-              {item.is_active ? (
-                <TiTick className="cardsContainer__tick-icon" />
-              ) : (
-                <AiOutlineClose className="cardsContainer__notAveilble-icon" />
-              )}
-            
-            </div>
-          ))}
-      </div>
-      <div className="cardsContainer__devider"></div>
-      <a className="cardsContainer__link">Get Started</a>
-    </div>
+          <div className="cardsContainer__pricePeriodContainer">
+            <p className="cardsContainer__exchangeRate">{symbol}</p>
+            <span className="cardsContainer__price">{price}</span>
+            <p className="cardsContainer__period">/ {period}</p>
+          </div>
+
+          <div className="cardsContainer__devider"></div>
+          <div className="cardsContainer__cardContentContainer">
+            {property &&
+              property.map((item, index) => (
+                <div className="cardsContainer__itemsContainer" key={index}>
+                  <p className="cardsContainer__items">{item.property_name}</p>
+                  {item.is_active ? (
+                    <TiTick className="cardsContainer__tick-icon" />
+                  ) : (
+                    <AiOutlineClose className="cardsContainer__notAveilble-icon" />
+                  )}
+                </div>
+              ))}
+          </div>
+          <div className="cardsContainer__devider"></div>
+          <a className="cardsContainer__link">Get Started</a>
+        </div>
+ 
+
   );
 };
 
