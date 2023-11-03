a86270151508cb5e117473d6b42aed6608e341a5
ramanboff <bayramofframan@gmail.com>
2023-11-04 00:50:59 +0400

    created render by date function

--- src/Components/Home/LatestNews/LatestNews.jsx
+++ src/Components/Home/LatestNews/LatestNews.jsx
@@ -1,22 +1,37 @@
+
 import { Link } from "react-router-dom";
 import "./LatestNews.scss"
 
 const LatestNews = ({ latestNews }) => {
+
+
+  const currentDate = new Date();
+  
+
+  const filteredNews = latestNews.filter((card) => {
+    const cardDate = new Date(card.date);
+  
+    return cardDate < currentDate && card.is_active;;
+  });
+
+  
   return (
   
 
    
  <div className="cards-container">
-      {latestNews.map((card) => (
+      {filteredNews.map((card) => (
+        <Link to={`/blogs/${card.slug}`} key={card.id}>
         <div className="card" key={card.id}>
           <div className="cardImgContainer">
             <img src={card.photo} />
           </div>
           <div className="bottom-content">
-            <h2>{card.company_name}</h2>
-            <p>{card.services_category}</p>
+            <h2>{card.title}</h2>
+            <p>{card.short_descriptions}</p>
           </div>
         </div>
+        </Link>
       ))}
     </div>
     
