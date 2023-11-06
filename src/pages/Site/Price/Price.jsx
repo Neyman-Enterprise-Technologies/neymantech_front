f631fcbb3feb03643dc5051116f366a27c21b0a7
ramanboff <bayramofframan@gmail.com>
2023-11-06 15:03:06 +0400

    price cards search by service_property

--- src/pages/Site/Price/Price.jsx
+++ src/pages/Site/Price/Price.jsx
@@ -4,13 +4,37 @@ import { Link } from "react-router-dom";
 import Loading from "../../../Components/Loading/Loading";
 import PriceCard from "../../../Components/PriceCard/PriceCard";
 import UseFetch from "../../../UseFetch";
-import { motion } from "framer-motion"
+import { BiCategory } from "react-icons/Bi";
+import { CiSearch } from "react-icons/ci";
+import { motion } from "framer-motion";
 import "./Price.scss";
 
 export default function Price() {
   const apiUrl = import.meta.env.VITE_API_URL;
 
   const { data: priceCards } = UseFetch(`${apiUrl}service_api/package/`);
+  const { data: price } = UseFetch(`${apiUrl}service_api/services_property/`);
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+   const handleMouseLeave = () => {
+    setHoveredItem(null);
+  };
+
+  const openModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
+  const [search, setSearch] = useState("");
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const handleClick = (id) => {
+    setSearch(id);
+    setIsModalOpen(false);
+  };
 
   const [loading, setLoading] = useState(false);
   useEffect(() => {
@@ -26,9 +50,11 @@ export default function Price() {
       {loading ? (
         <Loading />
       ) : (
-        <motion.div  initial={{ opacity: 0 }}
-        transition={{duration:0.5}}
-        whileInView={{ opacity: 1 }}>
+        <motion.div
+          initial={{ opacity: 0 }}
+          transition={{ duration: 0.5 }}
+          whileInView={{ opacity: 1 }}
+        >
           {/*       headerLink start */}
           <div className="priceHeader">
             <div className="container">
@@ -58,20 +84,85 @@ export default function Price() {
           </div>
           {/*    header end*/}
 
+          {isModalOpen && <div className="overlay" onClick={openModal} />}
+          <div className="modal-section">
+            <div className="kateqoriya-sec">
+              <div className="kateqoriya">
+                <div>
+                  <BiCategory className="category-icon" onClick={openModal} />
+                </div>
+                <p>Kateqoriya seçin</p>
+              </div>
+              <div className="input-search">
+                <input
+                  type="text"
+                  placeholder="search"
+                  onChange={handleSearch}
+                />
+                <div>
+                  <CiSearch
+                    className="category-search-icon"
+                    onClick={() => handleClick(card.services_property.title)}
+                  />
+                </div>
+              </div>
+            </div>
+
+            {isModalOpen && (
+              <motion.div
+                initial={{ opacity: 0 }}
+                transition={{ duration: 0.5 }}
+                whileInView={{ opacity: 1 }}
+                className="modal-wrapper"
+              >
+                <div className="modal">
+                  <ul>
+                    <li
+                      onMouseLeave={handleMouseLeave}
+                      onClick={() => handleClick('')}
+                    >
+                      All
+                      <FaAngleRight className="modal-angle-icon" />
+                    </li>
+                    {price &&
+                      price.map((card) => (
+                        <li
+                          key={card.id}
+                          onMouseEnter={() => handleMouseEnter(card.title)}
+                          onMouseLeave={handleMouseLeave}
+                          onClick={() => handleClick(card.id)}
+                        >
+                          {card.title}
+                          <FaAngleRight className="modal-angle-icon" />
+                        </li>
+                      ))}
+                  </ul>
+                </div>
+              </motion.div>
+            )}
+          </div>
+
           <div className="cardsContainer">
             {priceCards &&
-              priceCards.map((card) => (
-                <PriceCard
-                  title={card.package_name}
-                  price={card.price}
-                  period={card.price_period}
-                  serviceItems={card.property_name}
-                  color={card.color}
-                  property={card.package_properties}
-                  symbol={card.symbol}
-                  key={card.id}
-                />
-              ))}
+              priceCards
+                .filter((priceCard) => {
+                  if (search) {
+                    return priceCard?.services_property == search;
+                  }
+                  return true;
+                })
+                .map((card) => (
+                  <PriceCard
+                    title={card.package_name}
+                    price={card.price}
+                    period={card.price_period}
+                    serviceItems={card.property_name}
+                    color={card.color}
+                    property={card.package_properties}
+                    symbol={card.symbol}
+                    key={card.id}
+                  />
+                ))}
           </div>
         </motion.div>
       )}
