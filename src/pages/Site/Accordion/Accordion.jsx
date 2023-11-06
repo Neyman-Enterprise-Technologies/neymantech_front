3907010fd579f5c2d30f731d934b486cefdb510e
ramanboff <bayramofframan@gmail.com>
2023-11-06 19:41:41 +0400

    Accordion header style updated

--- src/pages/Site/Accordion/Accordion.jsx
+++ src/pages/Site/Accordion/Accordion.jsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from "react";
-import "./Accordion.scss"
+import "./Accordion.scss";
 import AccordionItem from "../../../Components/AccordionItem/AccordionItem";
 import UseFetch from "../../../UseFetch";
 import { Link } from "react-router-dom";
 import { FaAngleRight } from "react-icons/fa";
 import Loading from "../../../Components/Loading/Loading";
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 const Accordion = () => {
   const apiUrl = import.meta.env.VITE_API_URL;
   const { data: faq } = UseFetch(`${apiUrl}core_api/faq`);
@@ -20,47 +20,49 @@ const Accordion = () => {
     }, 1000);
   }, []);
   return (
-    <>  
-     {loading ? (
+    <>
+      {loading ? (
         <Loading />
       ) : (
-    <motion.div initial={{ opacity: 0 }}
-    transition={{duration:0.5}}
-    whileInView={{ opacity: 1 }}>
-    <div className="accordionHeader">
-    <div className="container">
-      <div className="accordionTitle">
-        <h2>
-          F<span>A</span>Q
-        </h2>
-      </div>
-    </div>
-    <div className="accordionLinks">
-      <Link to="/home" className="homeLink">
-        Home
-      </Link>
-      <FaAngleRight className="angelIcon" />
-      <Link className="faqLink" to="faq">FAQ</Link>
-    </div>
-  </div>
-    <div className="accordion">
-      {faq &&
-        faq.map((item, i) => (
-     
-          <AccordionItem
-            key={item.question}
-            
-            num={i}
-            title={item.question}
-            text={item.answer}
-            curOpen={curOpen}
-            onOpen={setCurOpen}
-          >
-            {item.answer}
-          </AccordionItem>
-        ))}
-    </div>
-    </motion.div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          transition={{ duration: 0.5 }}
+          whileInView={{ opacity: 1 }}
+        >
+          <div className="accordionHeader">
+            <div className="container">
+              <div className="accordionTitle">
+                <h2>
+                  F<span>A</span>Q
+                </h2>
+              </div>
+            </div>
+            <div className="accordionLinks">
+              <Link to="/home" className="homeLink">
+                Home
+              </Link>
+              <FaAngleRight className="angelIcon" />
+              <Link className="faqLink" to="faq">
+                FAQ
+              </Link>
+            </div>
+          </div>
+          <div className="accordion">
+            {faq &&
+              faq.map((item, i) => (
+                <AccordionItem
+                  key={item.question}
+                  num={i}
+                  title={item.question}
+                  text={item.answer}
+                  curOpen={curOpen}
+                  onOpen={setCurOpen}
+                >
+                  {item.answer}
+                </AccordionItem>
+              ))}
+          </div>
+        </motion.div>
       )}
     </>
   );
