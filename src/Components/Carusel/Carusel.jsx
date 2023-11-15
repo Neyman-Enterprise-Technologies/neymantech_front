1516827c3f4b4f3b4d846e3243f006f6b8273ce7
ramanboff <bayramofframan@gmail.com>
2023-11-15 22:45:39 +0400

    added className to feedback span and h3

--- src/Components/Carusel/Carusel.jsx
+++ src/Components/Carusel/Carusel.jsx
@@ -6,76 +6,74 @@ import "./Carusel.scss";
 
 const Carusel = ({ feedback }) => {
   return (
-   <div className="container">
-     <div>
-      <Swiper
-        spaceBetween={30}
-        navigation={{
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        }}
-        className="mySwiper"
-        breakpoints={{
-          640: {
-            slidesPerView: 1,
-            spaceBetween: 20,
-          },
-          768: {
-            slidesPerView: 2,
-            spaceBetween: 40,
-          },
-          1024: {
-            slidesPerView: 3,
-            spaceBetween: 50,
-          },
-        }}
-      >
-        {feedback.map(({ avatar, fullname, id, position, feedback }) => (
-          <SwiperSlide className="Slider" key={id}>
-          <div className="Slider__feedbackContainer">
-          <div className="Slider__feedback">{feedback}</div>
-          {/* <p className="Slider__feedback">fghjncvbjandfffffffffffffffffffffffffffffffffffffffffffffcccccccccccccccccccccvlsndvlnlnaldnlanlanljbvlkjsbvkjsbvljbsvlkjbslkvjbsv</p> */}
-            <div className="Slider__div">
-              <div>
-                <img src={avatar} alt="Slide 1" />
+    <div className="container">
+      <div>
+        <Swiper
+          spaceBetween={30}
+          navigation={{
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+          }}
+          className="mySwiper"
+          breakpoints={{
+            640: {
+              slidesPerView: 1,
+              spaceBetween: 20,
+            },
+            768: {
+              slidesPerView: 2,
+              spaceBetween: 40,
+            },
+            1024: {
+              slidesPerView: 3,
+              spaceBetween: 50,
+            },
+          }}
+        >
+          {feedback.map(({ avatar, fullname, id, position, feedback }) => (
+            <SwiperSlide className="Slider" key={id}>
+              <div className="Slider__feedbackContainer">
+                <div className="Slider__feedback">{feedback}</div>
+                <div className="Slider__avatarAndInfo">
+                  <div className="avatar-container">
+                    <img src={avatar} alt="Slide 1" />
+                  </div>
+                  <div className="nameAndPosition">
+                    <span className="name">{fullname}</span>
+                    <span className="position">{position}</span>
+                  </div>
+                </div>
               </div>
-              <div>
-                <h3>{fullname}</h3>
-                <span>{position}</span>
-              </div>
-            </div>
-          </div>
-          </SwiperSlide>
-        ))}
-        
-      </Swiper>
+            </SwiperSlide>
+          ))}
+        </Swiper>
 
-      {/* Sonraki üç slayt */}
-      <Swiper
-        spaceBetween={30}
-        navigation={{
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        }}
-        className="mySwiper"
-        breakpoints={{
-          640: {
-            slidesPerView: 1,
-            spaceBetween: 20,
-          },
-          768: {
-            slidesPerView: 2,
-            spaceBetween: 40,
-          },
-          1024: {
-            slidesPerView: 3,
-            spaceBetween: 50,
-          },
-        }}
-        initialSlide={3}
-      ></Swiper>
+        {/* Sonraki üç slayt */}
+        <Swiper
+          spaceBetween={30}
+          navigation={{
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+          }}
+          className="mySwiper"
+          breakpoints={{
+            640: {
+              slidesPerView: 1,
+              spaceBetween: 20,
+            },
+            768: {
+              slidesPerView: 2,
+              spaceBetween: 40,
+            },
+            1024: {
+              slidesPerView: 3,
+              spaceBetween: 50,
+            },
+          }}
+          initialSlide={3}
+        ></Swiper>
+      </div>
     </div>
-   </div>
   );
 };
 
